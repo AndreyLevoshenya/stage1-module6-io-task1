bf47ab951e6b1bee91e43b12b2e183363590a2d1
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-10 21:34:20 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder stringBuilder = new StringBuilder();
+        try(FileInputStream fileReader = new FileInputStream(file)) {
+            int ch;
+            while ((ch = fileReader.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            String[] lines = stringBuilder.toString().split("\n");
+            String regex = ": ";
+            String name = lines[0].trim().split(regex)[1];
+            Integer age = Integer.parseInt(lines[1].trim().split(regex)[1]);
+            String email = lines[2].trim().split(regex)[1];
+            Long phoneNumber = Long.parseLong(lines[3].trim().split(regex)[1]);
+
+            return new Profile(name, age, email, phoneNumber);
+        } catch (FileNotFoundException e) {
+            System.err.println("file not found");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        File file = new File("src/main/resources/Profile.txt");
+        Profile profile = fileReader.getDataFromFile(file);
+        System.out.println(profile);
     }
 }
